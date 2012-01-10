85ab284123b83cc65f17063bade289ee0f61fa5a
Burhan Khalid <burhan.khalid@gmail.com>
2012-01-11 00:15:07 +0300

    Initial upload of codebase

---
+++ knet/api.py
@@ -0,0 +1,304 @@
+# encoding: UTF-8
+"""
+ 
+ This is a pure python implementation of the e24PaymentPipe class
+ usually provided with ACI payment gateway modules.
+
+ The default samples provided with the gateway are only in Java,
+ ASP and ColdFusion so this class was created to provide an
+ implementation in pure Python.
+
+ @author Burhan Khalid <burhan.khalid@gmail.com>
+
+ This code is provided with no license restrictions.
+ You may use it, derive from it, modify it at your own peril.
+ The author is not responsible should the use of this code
+ lead to strange anomalies in the time space continuum.
+
+
+"""
+
+import cStringIO as StringIO
+import itertools
+import zipfile
+import httplib
+import urllib
+import string
+
+from xml.dom.minidom import parseString
+
+class AliasNotFound(Exception): pass
+class GatewayError(Exception): pass
+class InvalidResponse(Exception): pass
+
+
+class e24PaymentPipe():
+    """
+
+      This is the main class that defines the payment pipe. For historical
+      reasons, the name of this class matches that of the Java class.
+
+      However, this can easily be changed in any future revisions.
+
+      The class provides the following methods and properties.
+
+      See the example implementation for a working example.
+
+      Properties
+      ==========
+
+      ERROR_URL = A fully qualified URL that will be used in case there is an error with the transaction
+      RESPONSE_URL = The redirect URL after a successful response
+
+      Sample use:
+      from datetime import datetime
+      from e24PaymentPipe import e24PaymentPipe as gateway
+
+      gw = gateway('somefile.cgn','somealias')
+      try:
+        gw.parse()
+      except (zipfile.BadZipfile,AliasNotFound):
+        pass
+
+      gw.ERROR_URL = 'https://www.example.com/error.html'
+      gw.RESPONSE_URL = 'https://www.example.com/return.jsp'
+
+      trackid = "{0.year}{0.month}{0.day}{0.hour}{0.minute}".format(datetime.now())
+
+      try:
+        r = gw.transaction(trackid,amount=2.000)
+      except GatewayError:
+        pass
+
+      print('Payment ID: %s' % r[0])
+      print('Gateway URL: %s' % r[1])
+
+
+    """
+
+    ERROR_URL = None
+    RESPONSE_URL = None
+    ALIAS = None
+    RESOURCE = None
+
+    def __init__(self, resource, alias):
+        self._buffer = 2320 # Buffer for reading lines from the resource file
+        self._nodes = ('id', 'password', 'webaddress', 'port', 'context')
+        self._gw = dict() # Stores the various elements to create the gateway
+        self._action = 1 # For payment
+
+        self.RESOURCE = resource
+        self.ALIAS = alias
+
+    def _xor(self, s):
+        key = "Those who profess to favour freedom and yet depreciate agitation are men who want rain "
+        key += "without thunder and lightning"
+        key = itertools.cycle(key)
+        return ''.join(chr(ord(x) ^ ord(y)) for (x, y) in itertools.izip(s, key))
+
+
+    def parse(self):
+        """
+          
+          Method to parse the resource file for the terminal alias
+          provided, and populate the gw array with the terminal information.
+
+          Parameters
+          ==========
+             None
+
+          Exceptions
+          ==========
+
+             zipfile.BadZipfile - in case resource file cannot be read
+             AliasNotFound - if alias terminal not found in
+             resource file
+
+        """
+
+        out = StringIO.StringIO() # Temporary "file" to hold the zipped content
+        with open(self.RESOURCE, 'rb') as f:
+            out.write(self._xor(f.read(self._buffer)))
+
+        try:
+            temp = zipfile.ZipFile(out)
+        except zipfile.BadZipfile:
+            raise zipfile.BadZipfile
+
+        if self.ALIAS + ".xml" in temp.namelist():
+            t = temp.open(self.ALIAS + ".xml")
+            s = self._xor(''.join(f for f in t.read(self._buffer)))
+        else:
+            raise Exception(AliasNotFound, "%s not found in %s."
+            % (self.ALIAS, self.RESOURCE))
+
+        d = parseString(s) # Populate the DOM from the XML file
+
+        for node in self._nodes:
+            self._gw[node] = d.getElementsByTagName(node)[0].childNodes[0].nodeValue
+
+
+    def connect(self, params):
+        """
+
+          Performs the connection to the gateway to retrieve the
+          paymentid and gateway URL for submission of the payment request
+
+          Parameters
+          ==========
+
+            params - a dictionary of various parameters required to submit to
+            the payment gateway for a successful request.
+
+          Exceptions
+          ==========
+
+            httplib.HTTPException
+            httplib.NotConnected
+            InvalidResponse - invalid response received from the gateway
+
+          Returns
+          =======
+
+            Two member list, as a result of the gateway transaction
+
+        """
+
+        params = urllib.urlencode(params)
+
+        try:
+            if int(self._gw['port']) == httplib.HTTPS_PORT:
+                conn = httplib.HTTPSConnection(self._gw['webaddress'], httplib.HTTPS_PORT)
+            else:
+                conn = httplib.HTTPConnection(self._gw['webaddress'], httplib.HTTP_PORT)
+        except httplib.HTTPException:
+            raise Exception(httplib.HTTPException, 'Cannot open a connection to %s on port %s. Error was %s' %
+                                                   (self._gw['webaddress'], self._gw['port'], httplib.HTTPException))
+
+        try:
+            conn.connect()
+        except httplib.NotConnected:
+            raise Exception(httplib.NotConnected)
+
+        if self._gw['context'][-1] != '/':
+            context = self._gw['context'] + '/'
+        else:
+            context = self._gw['context']
+
+        context = '/' + context + 'servlet/PaymentInitHTTPServlet'
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+        try:
+            conn.request("POST", context, params, headers)
+        except httplib.HTTPException:
+            raise httplib.HTTPException
+
+        result = conn.getresponse()
+        data = result.read()
+
+        try:
+            return data.split(':', 1)
+        except KeyError:
+            raise Exception(InvalidResponse)
+
+
+    def transaction(self, trackid, udf=None, amount=1.000, lang='ENG', currency=414, filter=True):
+        """
+
+           Main method to initiate a transaction request for the gateway.
+
+           Parameters
+           ==========
+
+             amount - float, minimum 1, default 1.000
+
+             lang - code of the language to be used, defaults to 'ENG'
+
+             currency - ISO currency code, defaults to 414 for Kuwaiti Dinars
+
+             filter - Switch to filter UDF fields. Defaults to True
+
+             udf - a dictionary object containing UDF fields for the
+             transaction. See your gateway documentation on UDF fields. Format
+             is:
+
+                udf['udf1'] = 'some text'
+                udf['udf2'] = 'some text
+
+             trackid = a unique tracking id. Can be in any format, but must be
+             unique for each transaction request; and must not contain any characters
+             listed in the table "characters not allowed".
+
+             Returns
+             =======
+
+             Two member list containing the payment id, and the gateway URL
+
+             Exceptions
+             ==========
+
+             In case of an error, an exception is thrown with the text of the
+             error message.
+
+             Following characters are not allowed in UDF fields or trackid.
+             For UDF fields, the code will do a simple substitution with "-",
+             if you don't want this done, because you have already
+             taken care of this, call the method with 'filter=False'
+
+             Sym   Hex  Name
+             ===============
+             ~     x7E  TILDE
+             `     x60  LEFT SINGLE QUOTATION MARK, GRAVE ACCENT
+             !     x21  EXCLAMATION POINT (bang)
+             #     x23  NUMBER SIGN (pound sign)
+             $     x24  DOLLAR SIGN
+             %     x25  PERCENT SIGN
+             ^     x5E  CIRCUMFLEX ACCENT
+             |     x7C  VERTICAL LINE (pipe)
+             \     x5C  REVERSE SLANT (REVERSE SOLIDUS)(backslash, backslant)
+             :     x3A  COLON
+             '     x27  APOSTROPHE, RIGHT SINGLE QUOTATION MARK, ACUTE ACCENT (single quote)
+             "     x22  QUOTATION MARK, DIAERESIS
+             /     x2F  SLANT (SOLIDUS), (slash)
+        """
+
+        if self.ERROR_URL is None:
+            raise AttributeError('ERROR_URL not set.')
+
+        if self.RESPONSE_URL is None:
+            raise AttributeError('RESPONSE_URL not set.')
+
+        params = dict()
+        params['id'] = self._gw['id']
+        params['password'] = self._gw['password']
+        params['action'] = self._action
+
+        params['amt'] = amount
+        params['currencycode'] = currency
+        params['langid'] = lang
+        params['errorURL'] = self.ERROR_URL
+        params['responseURL'] = self.RESPONSE_URL
+        params['trackid'] = trackid
+
+        if udf is not None:
+            keys = udf.keys()
+            if filter:
+                s = "~`!#$%^|\:'\"/"
+                trans = string.maketrans(s, ''.join(['-'] * len(s)))
+                for k in keys:
+                    params[k.lower()] = udf[k].translate(trans)
+            else:
+                for k in keys:
+                    params[k.lower()] = udf[k]
+
+        try:
+            r = self.connect(params)
+        except KeyError:
+            return False
+        try:
+            if r[0][1:6] == 'ERROR':
+                raise Exception(GatewayError)
+        except KeyError:
+            raise Exception(GatewayError)
+
+        return r
---
+++ main.py
@@ -0,0 +1,169 @@
+#encoding: UTF-8
+"""
+A sample e24PaymentPipe client for KNET. Use this as inspiration
+(or directly in your code) when you need to provide a KNET payment
+gateway for your application.
+
+It has minimal requirements and is written in Flask using SQLAlchemy
+as the database ORM.
+
+"""
+
+from datetime import date
+from datetime import datetime
+from flask import Flask, request, redirect, render_template, flash
+from flask.helpers import url_for
+from flaskext.sqlalchemy import SQLAlchemy
+
+from knet.api import e24PaymentPipe as gw
+
+app = Flask(__name__)
+
+# Setting up a default database to be used for knet specific information
+# In production, you should probably change this to point to an existing database
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///knet.db'
+app.config['SECRET_KEY'] = 'changeme' # change this!
+db = SQLAlchemy(app)
+
+# Change these to match your server URLs
+# These are required for KNET, but can
+# be customized to your needs.
+
+ERROR_URL = 'http://127.0.0.1:5000/error/'
+SUCCESS_URL = 'http://127.0.0.1:5000/thank-you/'
+RESPONSE_URL = 'http://127.0.0.1:5000/result/'
+
+knet = gw('resource.cgn','alias')
+knet.ERROR_URL = ERROR_URL
+knet.RESPONSE_URL = RESPONSE_URL
+
+class Transaction(db.Model):
+    __tablename__ = 'KNET_TRANS'
+    id = db.Column(db.Integer, primary_key=True)
+    transaction_id = db.Column(db.String(50))
+    payment_id = db.Column(db.String(50))
+    tracking_id = db.Column(db.String(50))
+    reference = db.Column(db.String(50), default=None)
+    total = db.Column(db.Float, default=0.000)
+    result = db.Column(db.String(100))
+    postdate = db.Column(db.Date)
+    auth = db.Column(db.String(50))
+    order_id = db.Column(db.String(50))
+
+    def __init__(self,tracking,order,total=1.000):
+        self.tracking_id = tracking
+        self.total = total
+        self.order_id = order
+
+    def __repr__(self):
+        return '<KNET R: {0} | T: {1} | O: {2}>'.format(self.payment_id,self.total,self.order_id)
+
+@app.route('/error/<pid>')
+def error(pid=None):
+    if pid:
+        t = Transaction.query.filter_by(payment_id=pid).first()
+        flash('Oops! There was an error with your payment!')
+        return render_template('error.html',t=t)
+    else:
+        return 'There was an error with your request'
+
+@app.route('/thank-you/<pid>')
+def thanks(pid):
+    # Fetch the object for this payment ID
+    if not pid:
+       return 'ERROR'
+    else:
+       t = Transaction.query.filter_by(payment_id=pid).first()
+
+    flash('Your payment was successful')
+    return render_template('thanks.html',t=t)
+
+@app.route('/result/',methods=['POST'])
+def result():
+    # Get the paymentid
+    pid = request.form.get('paymentid')
+
+    t = Transaction.query.filter_by(payment_id=pid).first() or None
+    if not t:
+        return redirect(url_for('error'))
+
+    # We have the transaction, now fill in the rest of the fields
+
+    r = request.form.get('result')
+
+    t.result = r
+    t.postdate = date(date.today().year,int(request.form.get('postdate')[:2]),int(request.form.get('postdate')[2:]))
+    t.transaction_id = request.form.get('tranid')
+    t.tracking_id = request.form.get('trackid')
+    t.reference = request.form.get('ref')
+    t.auth = request.form.get('auth')
+
+    # Store the result
+    db.session.add(t)
+    db.session.commit()
+
+    if r == unicode('CANCELLED') or r == unicode('NOT CAPTURED'):
+        return 'REDIRECT=%s%s' % (ERROR_URL,pid)
+
+    return 'REDIRECT=%s%s' % (SUCCESS_URL,pid)
+
+@app.route('/<id>/<total>/<trackingid>/<udf>')
+@app.route('/<id>/<total>/<trackingid>/')
+@app.route('/<id>/<total>/')
+def entry(id,trackingid=None,total=1.000,udf=None):
+
+    """
+    This is the main entry point for the server, you pass it three things:
+
+    1. The ID of the transaction from your database - this could be a receipt
+       number or any other unique identifier.
+
+    2. The amount that needs to be paid. Minimum is 1, which is the default.
+
+    3. A unique tracking ID. This is optional, but highly recommended. KNET
+       requires this for their backend. If you don't pass one, the system
+       generates one with the following pattern:
+
+       YYYYMMDD-ID-HHMMSS
+
+    5. Optional UDF fields (1-5)
+
+    Example URL:
+
+    /12/34.345/ABC-TRACK/abc,foo@bar.com,hello
+
+    12 = ID
+    34.345 = Amount to be charged
+    ABC-TRACK = Tracking ID
+    abc = UDF field 1
+    foo@bar.com = UDF field 2
+    hello = UDF field 3
+
+    Restrictions - none of your fields can include the / character
+    """
+    # - Check if this order has already been paid
+    t = Transaction.query.filter_by(order_id=id,result='CAPTURED').first()
+    if t is not None:
+        return 'You have already paid for this order.'
+
+    # 2 - Build and dispatch KNET URL
+    trackid = trackingid or '{0.year}{0.month}{0.day}-{1}-{0.hour}{0.minute}{0.second}'.format(datetime.now(),id)
+
+    knet.parse()
+    if udf is not None:
+        udf = {'udf%s'%udf.find(x):x for x in udf if x is not ','}
+    payment_id,payment_url = knet.transaction(trackid,amount=total,udf=udf)
+
+    # Store information in DB
+    t = Transaction(trackid,id,total)
+    t.payment_id = payment_id
+    db.session.add(t)
+    db.session.commit()
+    return redirect(payment_url+'?PaymentID='+payment_id)
+
+if __name__ == '__main__':
+    db.create_all() # This will reset and recreate the database on each restart of the system
+    app.run(host='0.0.0.0',debug=True)
+
+
