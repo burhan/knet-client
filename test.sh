c07e93a95f845f8ec81a6c060530f445e7667c88
Yatharth Agarwal <yatharth999@gmail.com>
2014-03-26 22:01:32 +0300

    Revert "Improve test suite for contributors"
    This reverts commit a7a24d0299eeb9dc56000ed1916141fa9589fb8d.

--- test.sh
+++ test.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-# if ls Contributors.txt > /dev/null
-# then
-#     echo "Passed Contributors.txt test"
-# else
-#     echo "Failed the Contributors.txt test"
-#     exit 1
-# fi
-
-import glob
-files = glob.glob("CONTRIBUTORS.TXT")
-assert(files)
+if ls Contributors.txt > /dev/null
+then
+    echo "Passed Contributors.txt test"
+else
+    echo "Failed the Contributors.txt test"
+    exit 1
+fi
