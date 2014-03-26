a7a24d0299eeb9dc56000ed1916141fa9589fb8d
Yatharth Agarwal <yatharth999@gmail.com>
2014-03-26 21:58:05 +0300

    Improve test suite for contributors

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if ls Contributors.txt > /dev/null
-then
-    echo "Passed Contributors.txt test"
-else
-    echo "Failed the Contributors.txt test"
-    exit 1
-fi
+# if ls Contributors.txt > /dev/null
+# then
+#     echo "Passed Contributors.txt test"
+# else
+#     echo "Failed the Contributors.txt test"
+#     exit 1
+# fi
+
+import glob
+files = glob.glob("CONTRIBUTORS.TXT")
+assert(files)
